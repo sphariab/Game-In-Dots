26394c4a4f5e4b3366edacfb57fe3a0af6e42604
sphariab@gmail.com <sphariab@gmail.com>
2020-05-22 14:09:25 +0300

    Updates

---
+++ precache-manifest.b9a6a7f1428e468427352b12306758e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8df3183ce8b42482a78c4734c5e5b39",
+    "url": "/Game-In-Dots/index.html"
+  },
+  {
+    "revision": "ad6581560e69daf2bfd4",
+    "url": "/Game-In-Dots/static/js/2.7014c14d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Game-In-Dots/static/js/2.7014c14d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bd16722ca1ada1a70e0",
+    "url": "/Game-In-Dots/static/js/main.faf1395b.chunk.js"
+  },
+  {
+    "revision": "7f54b4d202eb9c710a13",
+    "url": "/Game-In-Dots/static/js/runtime-main.f1db7bf2.js"
+  }
+]);
